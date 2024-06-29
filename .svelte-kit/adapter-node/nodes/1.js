d6b21be1ae28f6bcd9668a76e006ff6590a2d141
BluTaiga <tjbermant@gmail.com>
2024-06-28 18:27:02 -0700

    Updates .gitignore to ignore node_modules and .svelte-kit

--- .svelte-kit/adapter-node/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.bgIjqY7Z.js","_app/immutable/chunks/scheduler.KIPEeUFd.js","_app/immutable/chunks/index.CJzG2D__.js","_app/immutable/chunks/entry.D9C0CAoD.js"];
-export const stylesheets = [];
-export const fonts = [];
--- .svelte-kit/adapter-node/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.BdskzvWk.js","_app/immutable/chunks/scheduler.KIPEeUFd.js","_app/immutable/chunks/index.CJzG2D__.js"];
-export const stylesheets = [];
-export const fonts = [];
