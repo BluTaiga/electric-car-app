0a0f15f7cf27f4ba9cfc7dfc9a05a4da4b804743
BluTaiga <tjbermant@gmail.com>
2024-07-07 10:00:01 -0700

    working on an updateVehicle api request

--- src/lib/graphql.js
+++ src/lib/graphql.js
@@ -24,6 +24,16 @@ export const typeDefs = gql`
       summary: String
     ): Vehicle
   }
+  type Mutation{
+    updateVehicle(
+      name: String!,
+      price: Float!,
+      batterySize: Float!,
+      chargingSpeed: Float!,
+      imageName: String,
+      summary: String
+    ): Vehicle
+  }
 `;
 
 export const resolvers = {
@@ -41,6 +51,30 @@ export const resolvers = {
       }
       const newCar = new Car({ name, price, batterySize, chargingSpeed, imageName, summary });
       return await newCar.save();
+    },
+    updateVehicle: async (_, { _id, name, price, batterySize, chargingSpeed, imageName, summary }, context) => {
+      console.log('Resolver context:', context); //Debug log
+      if (!context.authenticated || !context.user || context.user.role !== 'admin') {
+        console.log('Not authorized - Auth:', context.authenticated, 'User:', context.user); // Debug log
+        throw new Error('Not authorized');
+      }
+
+      try {
+        const updateCar = await Car.findByIdAndUpdate(
+          _id,
+          { name, price, batterySize, chargingSpeed, imageName, summary },
+          { new: true }
+        );
+
+        if (!updatedCar) {
+          throw new Error('Vehicle not found');
+        }
+
+        return updatedCard;
+      } catch (error) {
+        console.error('Error updating vehicle:', error);
+        throw new Error('Failed to update vehicle')
+      }
     }
   }
 };
